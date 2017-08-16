a6fc117cbd84aed222e9c247606e5ce771c7daf5
XiNGRZ <chenxingyu92@gmail.com>
2017-08-16 23:23:41 +0800

    ConfigPanel: Handle some edge cases for KeyHandler
    * Only restore controller state while there's really a controller
    * Only handles ACTION_UP from slider to avoid processing the event twice
    
    Change-Id: I7e16d8f8b4f0302c70e00a1562107df8570d61e6

--- keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -138,27 +138,25 @@ public class KeyHandler implements DeviceKeyHandler {
             switch (usage) {
                 case NotificationController.ID:
                     mSliderController = mNotificationController;
-                    mSliderController.update(actions);
                     break;
                 case FlashlightController.ID:
                     mSliderController = mFlashlightController;
-                    mSliderController.update(actions);
                     break;
                 case BrightnessController.ID:
                     mSliderController = mBrightnessController;
-                    mSliderController.update(actions);
                     break;
                 case RotationController.ID:
                     mSliderController = mRotationController;
-                    mSliderController.update(actions);
                     break;
                 case RingerController.ID:
                     mSliderController = mRingerController;
-                    mSliderController.update(actions);
                     break;
             }
-
-            mSliderController.restoreState();
+         
+            if (mSliderController != null) {
+                mSliderController.update(actions);
+                mSliderController.restoreState();
+            }
         }
     };
 
@@ -294,6 +292,10 @@ public class KeyHandler implements DeviceKeyHandler {
     }
 
     public boolean handleKeyEvent(KeyEvent event) {
+        if (event.getAction() != KeyEvent.ACTION_UP) {
+            return false;
+        }
+
         int scanCode = event.getScanCode();
         boolean isKeySupported = scanCode == FLIP_CAMERA_SCANCODE;
         boolean isSliderControllerSupported = mSliderController != null &&
